49615e3b389ca55e901b8e252619c33efce60bb3
snaevartor <snaevartor@gmail.com>
2017-01-25 13:31:45 +0000

    Second commit. Testing minor code changes.

--- Hotel3H/src/hotel3h/Hotel3H.java
+++ Hotel3H/src/hotel3h/Hotel3H.java
@@ -17,6 +17,7 @@ public class Hotel3H {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, hotel!");
+        System.out.println(2+2);
     }
     
 }
