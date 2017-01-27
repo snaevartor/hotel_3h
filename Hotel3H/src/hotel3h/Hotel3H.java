bf6913ee3cbef7e00c2ea1fe5ae7f54d17886f6c
snaevartor <snaevartor@gmail.com>
2017-01-27 21:11:27 +0000

    Added a new test database along with code for selecting and displaying data from it.
    Update also includes a .sql file used for generating a new, clean version of this database, in case it is corrupted. (Not used in the current code, but may be incorporated into future updates.)

--- Hotel3H/src/hotel3h/Hotel3H.java
+++ Hotel3H/src/hotel3h/Hotel3H.java
@@ -16,15 +16,34 @@ public class Hotel3H {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello, hotel!");
-        System.out.println(2+2);
-
-        System.out.println("Java/SQL Interface");
+        System.out.println("Hotel 3H - Java/SQL Interface");
         Connection c = null;
+        Statement stmt = null;
         try {
+            //c.setAutoCommit(false);
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c = DriverManager.getConnection("jdbc:sqlite:src/sql/hotel3h.db");
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM Hotels;" );
+            
+            System.out.println("ID - Name - Type - Stars - Country - City - Rooms - HasPool");
+            
+            while ( rs.next() ) {
+                int id = rs.getInt("ID");
+                String name = rs.getString("Name");
+                String type = rs.getString("Type");
+                int stars = rs.getInt("Stars");
+                String country = rs.getString("Country");
+                String city = rs.getString("City");
+                int rooms = rs.getInt("Rooms");
+                boolean haspool = rs.getBoolean("HasPool");
+                System.out.println(id + " | " + name + " | " + type + " | " + stars + " | " + country + " | " + city + " | " + rooms + " | " + haspool);
+            }
+            
+            rs.close();
+            stmt.close();
+            c.close(); 
+            
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
