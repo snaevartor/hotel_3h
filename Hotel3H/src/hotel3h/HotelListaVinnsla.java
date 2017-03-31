c4c4790ba2ebe7a678a532ed7609d36e450dff88
Soley Benediktsdottir <soleyben@gmail.com>
2017-03-31 12:33:23 +0000

    Uppfærsla 31.mars kl 12:26 Leita eftir herbergisfjölda líka

--- Hotel3H/src/hotel3h/HotelListaVinnsla.java
+++ Hotel3H/src/hotel3h/HotelListaVinnsla.java
@@ -16,21 +16,26 @@ import javax.swing.JList;
 public class HotelListaVinnsla extends DefaultListModel{
     ArrayList<Hotel> hotel;
     DefaultListModel listiRoom = new DefaultListModel();  //New Default list model
+    int sort;
     
     public HotelListaVinnsla(ArrayList<Hotel> hl, int sort){
         //Add all elements from the ArrayList to the list
         hotel = hl;
-        if(sort == 1){
-            Collections.sort(hotel);     
-        }
+        this.sort = sort;
+        sortHotels();
         for(Hotel h: hotel){
             addElement(h.getName());
         }   
     }
     //Adds one element to the list
-    public void addtoList(Hotel hl){
+/*    public void addtoList(Hotel hl){
         addElement(hl.getName());
     }
-    
+*/    
+    public void sortHotels(){
+        if(sort==1){
+            Collections.sort(hotel);
+        }
+    }
     
 }
