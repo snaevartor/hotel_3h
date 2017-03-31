a6bed7119817d3eb8c9f2f5d0284d783813123b5
snaevartor <snaevartor@gmail.com>
2017-03-31 11:21:09 +0000

    Cleaned-up code for the HDM unit test class.

--- Hotel3H/test/hotel3h/HotelDatabaseManagerTest.java
+++ Hotel3H/test/hotel3h/HotelDatabaseManagerTest.java
@@ -9,7 +9,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Unit Test class for HotelDatabaseManager
+ * 
  * @author Snaevar
  */
 public class HotelDatabaseManagerTest {
@@ -34,12 +35,13 @@ public class HotelDatabaseManagerTest {
     }
 
     /**
-     * Test of searchRooms method, of class HotelDatabaseManager.
+     * Test of 1 searchRooms method, of class HotelDatabaseManager.
+     * Should return the rooms from the first hotel in the database.
      */
     @Test
     public void testSearchRooms() {
         System.out.println("searchRooms");
-        int hnr = 1; //should return the rooms from hotel nr 1.
+        int hnr = 1;
         int pets = 0;
         int washing = 0;
         int kitchen = 0;
@@ -60,23 +62,18 @@ public class HotelDatabaseManagerTest {
         HotelDatabaseManager instance = new HotelDatabaseManager();
         boolean expResult = false;
         ArrayList<Room> testResult = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
-        boolean result = testResult.isEmpty();
-//        ArrayList<Room> expResult = new ArrayList<>();
-//        ArrayList<Room> expResult = null;
-//        ArrayList<Room> result = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);        
-//        System.out.println(expResult);
-//        System.out.println(result);
-        
+        boolean result = testResult.isEmpty();        
         assertEquals(expResult, result);
     }
     
     /**
      * Test 2 of searchRooms method, of class HotelDatabaseManager.
+     * Since hotel id in the database start at 1, this should return an empty array list.
      */
     @Test
     public void testSearchRooms2() {
         System.out.println("searchRooms2");
-        int hnr = 0; //hotel ids in database start at '1', so this should return an empty array list.
+        int hnr = 0;
         int pets = 0;
         int washing = 0;
         int kitchen = 0;
@@ -96,22 +93,18 @@ public class HotelDatabaseManagerTest {
         int baby = 0;
         HotelDatabaseManager instance = new HotelDatabaseManager();
         boolean expResult = true;
-//        ArrayList<Room> expResult = new ArrayList<>();        
         ArrayList<Room> testResult = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
         boolean result = testResult.isEmpty();
-//        System.out.println(testResult.isEmpty());
-//        System.out.println(testResult.get(0));
-//        ArrayList<Room> expResult = null;
-  //      ArrayList<Room> result = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
         assertEquals(expResult, result);
     }
     
     /**
-     * Test of searchHotels method, of class HotelDatabaseManager.
+     * Test 1 of searchHotels method, of class HotelDatabaseManager.
+     * Should return an array list containing all hotels in database.
      */
     @Test
     public void testSearchHotels() {
-        System.out.println("searchHotels"); // should return all hotels.
+        System.out.println("searchHotels");
         int type = 0;
         int gym = 0;
         int spa = 0;
@@ -129,26 +122,21 @@ public class HotelDatabaseManagerTest {
         int elevator = 0;
         int flyBus = 0;
         HotelDatabaseManager instance = new HotelDatabaseManager();
-
-//        boolean expResult = false;
         int expResult = 10;
         ArrayList<Hotel> testResult = instance.searchHotels(type, gym, spa, pool, hottub, wifi, conference, restaurant, bar, inclusive, breakfast, cancellation, roomService, wheelChair, elevator, flyBus);        
-//        boolean result = testResult.isEmpty();
         int result = testResult.size();
-//        System.out.println(testResult.size());
-//        System.out.println(testResult.get(0).getNr());
-//        System.out.println(testResult.get(0).getName());
         assertEquals(expResult, result);
     }
 
     
     /**
-     * Test of searchHotels method, of class HotelDatabaseManager.
+     * Test 2 of searchHotels method, of class HotelDatabaseManager.
+     * Should return no hotels (empty array list) as no hotels currently meet specified conditions.
      */
     @Test
     public void testSearchHotels2() {
-        System.out.println("searchHotels2"); // should return no hotels.
-        int type = 1;
+        System.out.println("searchHotels2");
+        int type = 3;
         int gym = 1;
         int spa = 1;
         int pool = 1;
@@ -171,7 +159,8 @@ public class HotelDatabaseManagerTest {
         assertEquals(expResult, result);
     }
     /**
-     * Test of getHotelStringInfo method, of class HotelDatabaseManager.
+     * Test 1 of getHotelStringInfo method, of class HotelDatabaseManager.
+     * Should return the string format information about the first hotel in the database.
      */
     @Test
     public void testGetHotelStringInfo() {
@@ -184,7 +173,7 @@ public class HotelDatabaseManagerTest {
     }
 
     /**
-     * Test of getHotelStringInfo method, of class HotelDatabaseManager.
+     * Test 2 of getHotelStringInfo method, of class HotelDatabaseManager.
      */
     @Test
     public void testGetHotelStringInfo2() {
@@ -197,20 +186,20 @@ public class HotelDatabaseManagerTest {
     }
 
     /**
-     * Test of getHotelStringInfo method, of class HotelDatabaseManager.
+     * Test 2 of getHotelStringInfo method, of class HotelDatabaseManager.
+     * Should throw an error, since the input is invalid.
      */
     @Test(expected=NullPointerException.class)
     public void testGetHotelStringInfo3() {
         System.out.println("getHotelStringInfo3"); //should result in an error
         Integer nr = null;
         HotelDatabaseManager instance = new HotelDatabaseManager();
- //       String[] expResult = {"Hotel Ódinsvé","Thorsgata 1, 101 Reykjavík, Iceland","www.hotelodinsve.com"};
         String[] result = instance.getHotelStringInfo(nr);
-//        assertArrayEquals(expResult, result);
     }
     
     /**
-     * Test of getHotelIntInfo method, of class HotelDatabaseManager.
+     * Test 1 of getHotelIntInfo method, of class HotelDatabaseManager.
+     * Should return the integer format information about the first hotel in the database.
      */
     @Test
     public void testGetHotelIntInfo() {
@@ -223,7 +212,8 @@ public class HotelDatabaseManagerTest {
     }
 
     /**
-     * Test of getHotelIntInfo method, of class HotelDatabaseManager.
+     * Test 2 of getHotelIntInfo method, of class HotelDatabaseManager.
+     * Should return the integer format information about the last hotel in the database.
      */
     @Test
     public void testGetHotelIntInfo2() {
@@ -235,14 +225,30 @@ public class HotelDatabaseManagerTest {
         assertArrayEquals(expResult, result);
     }
 
+    /**
+     * Test 3 of getHotelIntInfo method, of class HotelDatabaseManager.
+     * Should throw an error, since the input is invalid.
+     */
     @Test(expected=NullPointerException.class)
     public void testGetHotelIntInfo3() {
         System.out.println("getHotelIntInfo3");
         Integer nr = null;
         HotelDatabaseManager instance = new HotelDatabaseManager();
-//        int[] expResult = {0,0};
         int[] result = instance.getHotelIntInfo(nr);
-//        assertArrayEquals(expResult, result);
     }
 
+    /**
+     * Test 4 of getHotelIntInfo method, of class HotelDatabaseManager.
+     * Should return an array of all zeroes, since -1 doesn't refer to any hotel.
+     */
+    @Test
+    public void testGetHotelIntInfo4() {
+        System.out.println("getHotelIntInfo4");
+        int nr = -1;
+        HotelDatabaseManager instance = new HotelDatabaseManager();
+        int[] expResult = {0,0};
+        int[] result = instance.getHotelIntInfo(nr);
+        assertArrayEquals(expResult, result);
+    }
+ 
 }
