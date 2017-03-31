9ab043dd77b8bdbdd2679ca15bef97eb21b9fc82
snaevartor <snaevartor@gmail.com>
2017-03-31 10:31:41 +0000

    Another update for the Unit Test for HotelDatabaseManager class.

--- Hotel3H/test/hotel3h/HotelDatabaseManagerTest.java
+++ Hotel3H/test/hotel3h/HotelDatabaseManagerTest.java
@@ -39,7 +39,7 @@ public class HotelDatabaseManagerTest {
     @Test
     public void testSearchRooms() {
         System.out.println("searchRooms");
-        int hnr = 1;
+        int hnr = 1; //should return the rooms from hotel nr 1.
         int pets = 0;
         int washing = 0;
         int kitchen = 0;
@@ -51,16 +51,22 @@ public class HotelDatabaseManagerTest {
         int smoke = 0;
         int ac = 0;
         int minPrice = 0;
-        int maxPrice = 0;
+        int maxPrice = 1000000;
         int minSize = 0;
-        int maxSize = 0;
+        int maxSize = 1000000;
         int bed1 = 0;
         int bed2 = 0;
         int baby = 0;
         HotelDatabaseManager instance = new HotelDatabaseManager();
-        ArrayList<Room> expResult = new ArrayList<>();
+        boolean expResult = false;
+        ArrayList<Room> testResult = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
+        boolean result = testResult.isEmpty();
+//        ArrayList<Room> expResult = new ArrayList<>();
 //        ArrayList<Room> expResult = null;
-        ArrayList<Room> result = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
+//        ArrayList<Room> result = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);        
+//        System.out.println(expResult);
+//        System.out.println(result);
+        
         assertEquals(expResult, result);
     }
     
@@ -70,7 +76,7 @@ public class HotelDatabaseManagerTest {
     @Test
     public void testSearchRooms2() {
         System.out.println("searchRooms2");
-        int hnr = 1;
+        int hnr = 0; //hotel ids in database start at '1', so this should return an empty array list.
         int pets = 0;
         int washing = 0;
         int kitchen = 0;
@@ -89,13 +95,14 @@ public class HotelDatabaseManagerTest {
         int bed2 = 0;
         int baby = 0;
         HotelDatabaseManager instance = new HotelDatabaseManager();
-        ArrayList<Room> expResult = new ArrayList<>();
-        
+        boolean expResult = true;
+//        ArrayList<Room> expResult = new ArrayList<>();        
         ArrayList<Room> testResult = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
-        System.out.println(testResult.isEmpty());
+        boolean result = testResult.isEmpty();
+//        System.out.println(testResult.isEmpty());
 //        System.out.println(testResult.get(0));
 //        ArrayList<Room> expResult = null;
-        ArrayList<Room> result = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
+  //      ArrayList<Room> result = instance.searchRooms(hnr, pets, washing, kitchen, minifridge, tv, bath, view, noise, smoke, ac, minPrice, maxPrice, minSize, maxSize, bed1, bed2, baby);
         assertEquals(expResult, result);
     }
     
@@ -104,7 +111,7 @@ public class HotelDatabaseManagerTest {
      */
     @Test
     public void testSearchHotels() {
-        System.out.println("searchHotels");
+        System.out.println("searchHotels"); // should return all hotels.
         int type = 0;
         int gym = 0;
         int spa = 0;
@@ -123,15 +130,46 @@ public class HotelDatabaseManagerTest {
         int flyBus = 0;
         HotelDatabaseManager instance = new HotelDatabaseManager();
 
-        boolean expResult = false;
+//        boolean expResult = false;
+        int expResult = 10;
         ArrayList<Hotel> testResult = instance.searchHotels(type, gym, spa, pool, hottub, wifi, conference, restaurant, bar, inclusive, breakfast, cancellation, roomService, wheelChair, elevator, flyBus);        
-        boolean result = testResult.isEmpty();
-        System.out.println(testResult.size());
-        System.out.println(testResult.get(0).getNr());
-        System.out.println(testResult.get(0).getName());
+//        boolean result = testResult.isEmpty();
+        int result = testResult.size();
+//        System.out.println(testResult.size());
+//        System.out.println(testResult.get(0).getNr());
+//        System.out.println(testResult.get(0).getName());
         assertEquals(expResult, result);
     }
 
+    
+    /**
+     * Test of searchHotels method, of class HotelDatabaseManager.
+     */
+    @Test
+    public void testSearchHotels2() {
+        System.out.println("searchHotels2"); // should return no hotels.
+        int type = 1;
+        int gym = 1;
+        int spa = 1;
+        int pool = 1;
+        int hottub = 1;
+        int wifi = 1;
+        int conference = 1;
+        int restaurant = 1;
+        int bar = 1;
+        int inclusive = 1;
+        int breakfast = 1;
+        int cancellation = 1;
+        int roomService = 1;
+        int wheelChair = 1;
+        int elevator = 1;
+        int flyBus = 1;
+        HotelDatabaseManager instance = new HotelDatabaseManager();
+        int expResult = 0;
+        ArrayList<Hotel> testResult = instance.searchHotels(type, gym, spa, pool, hottub, wifi, conference, restaurant, bar, inclusive, breakfast, cancellation, roomService, wheelChair, elevator, flyBus);        
+        int result = testResult.size();
+        assertEquals(expResult, result);
+    }
     /**
      * Test of getHotelStringInfo method, of class HotelDatabaseManager.
      */
@@ -156,6 +194,19 @@ public class HotelDatabaseManagerTest {
         String[] expResult = {"Hotel Ódinsvé","Thorsgata 1, 101 Reykjavík, Iceland","www.hotelodinsve.com"};
         String[] result = instance.getHotelStringInfo(nr);
         assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of getHotelStringInfo method, of class HotelDatabaseManager.
+     */
+    @Test(expected=NullPointerException.class)
+    public void testGetHotelStringInfo3() {
+        System.out.println("getHotelStringInfo3"); //should result in an error
+        Integer nr = null;
+        HotelDatabaseManager instance = new HotelDatabaseManager();
+ //       String[] expResult = {"Hotel Ódinsvé","Thorsgata 1, 101 Reykjavík, Iceland","www.hotelodinsve.com"};
+        String[] result = instance.getHotelStringInfo(nr);
+//        assertArrayEquals(expResult, result);
     }
     
     /**
