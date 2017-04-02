18cfa14f102bc5f5feb35346aa34d839f4ef7276
snaevartor <snaevartor@gmail.com>
2017-04-02 20:42:44 +0000

    Code cleanup for legibility in HotelDatabaseManager.

--- Hotel3H/src/hotel3h/HotelDatabaseManager.java
+++ Hotel3H/src/hotel3h/HotelDatabaseManager.java
@@ -1,3 +1,9 @@
+/**
+* Class: HotelDatabaseManager
+* Usage: Create and operate on Hotel objects.
+* Authors: Helgi, Omar
+*/
+
 package hotel3h;
 
 import java.sql.Connection;
@@ -9,28 +15,75 @@ import java.util.ArrayList;
 
 public class HotelDatabaseManager {
 	
+    /**
+    * Constructor for HotelDatabaseManager
+    */
     public  HotelDatabaseManager(){ }
+
+    /**
+    * Private method for establishing a connection with the database.
+    */
     private static Connection connect() {
         Connection conn = null;
         try {
-	// db parameters
             String url = "jdbc:sqlite:src/sql/hoteltest11.db";
-	// create a connection to the database
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return conn;          
     }
-	 
-    public ArrayList<Room> searchRooms(int hnr, int pets,
-	 int washing, int kitchen, int minifridge, int tv,
-	 int bath, int view, int noise, int smoke, int ac, 
-	 int minPrice, int maxPrice, int minSize, int maxSize,
-	 int bed1, int bed2, int baby){	 			
-            ArrayList<Room> rooms = new ArrayList<>();
-	    String sql ;
-            sql = "SELECT * FROM RoomSearch, Room WHERE Room.nr = Roomsearch.nr AND"
+
+    /**
+    * Private method for getting a list of rooms belonging to a specific hotel.
+    */
+    private ArrayList<Room> getRooms(int hnr){
+        String sql = "SELECT * FROM RoomSearch, Room WHERE RoomSearch.nr = Room.nr AND hotelnr = hnr AND hnr = " + hnr;
+        ArrayList<Room> rooms = new ArrayList<>();	
+        try (Connection conn = connect();
+        Statement stmt  = conn.createStatement();
+        ResultSet rs    = stmt.executeQuery(sql)){	     
+            while (rs.next()) {
+            	Room r = new Room(rs.getInt("nr"), rs.getInt("hnr"), rs.getInt("pets"), 
+                    rs.getInt("washing"), rs.getInt("kitchen"), rs.getInt("minifridge"),
+	            rs.getInt("tv"), rs.getInt("bath"), rs.getInt("view"), rs.getInt("view"),
+	            rs.getInt("noise"), rs.getInt("smoke"), rs.getInt("ac"), rs.getInt("price"), 
+	            rs.getInt("size"), rs.getInt("bed1"), rs.getInt("bed2"), rs.getInt("baby"));
+	            rooms.add(r);
+	    }
+		
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return rooms;	      
+    }
+
+    /**
+    * Filter rooms based on specified search criteria.
+    */
+    public ArrayList<Room> searchRooms(
+            int hnr,
+            int pets,
+            int washing,
+            int kitchen,
+            int minifridge,
+            int tv,
+            int bath,
+            int view,
+            int noise,
+            int smoke,
+            int ac,
+            int minPrice,
+            int maxPrice,
+            int minSize,
+            int maxSize,
+            int bed1,
+            int bed2,
+            int baby){
+        
+                ArrayList<Room> rooms = new ArrayList<>();
+                String sql;
+                sql = "SELECT * FROM RoomSearch, Room WHERE Room.nr = Roomsearch.nr AND"
       		+ " hotelnr = hnr AND hnr = " + hnr + 
        		" AND pets >= " + pets + " AND washing >= " + washing +
        		" AND kitchen >= " + kitchen + " AND minifridge >= " + minifridge +
@@ -56,32 +109,47 @@ public class HotelDatabaseManager {
         }
         return rooms;
     }
-	 
-    public ArrayList<Hotel> searchHotels(int type,
-	 int gym, int spa, int pool, int hottub, 
-	 int wifi, int conference, int restaurant,
-	 int bar, int inclusive, int breakfast,
-	 int cancellation, int roomService, int wheelChair,
-	 int elevator, int flyBus){ 	
-        ArrayList<Hotel> hotels = new ArrayList<>();
-	String sql ;
-//      System.out.println("Komin í línu 94");
-	sql = "SELECT * FROM HotelSearch, Hotel WHERE HotelSearch.nr = Hotel.nr AND "
-            + "gym >= " + gym + " AND spa >= " + spa +
-            " AND pool >= " + pool + " AND hottub >= " + hottub +
-            " AND wifi >= " + wifi + " AND conference >= " + conference +
-            " AND restaurant >= " + restaurant + " AND bar >= " + bar +
-            " AND inclusive >= " + inclusive + " AND breakfast >= " + breakfast +
-            " AND cancellation >= " + cancellation + " AND roomService >= " + roomService +
-            " AND wheelChair >= " + wheelChair + " AND elevator >= " + elevator +
-            " AND flyBus >= " + flyBus;
-        if (type != 0)
-            sql += " AND type = " + type;
-//              System.out.println("Komin í línu 106");
-	    try (Connection conn = connect();
-            Statement stmt  = conn.createStatement();
-            ResultSet rs    = stmt.executeQuery(sql)){
-//	System.out.println("Komin í línu 112");         
+
+    /**
+    * Filter hotels based on specified search criteria.
+    */
+    public ArrayList<Hotel> searchHotels(
+            int type,
+            int gym,
+            int spa,
+            int pool,
+            int hottub,
+            int wifi,
+            int conference,
+            int restaurant,
+            int bar,
+            int inclusive,
+            int breakfast,
+            int cancellation,
+            int roomService,
+            int wheelChair,
+            int elevator,
+            int flyBus){
+        
+                ArrayList<Hotel> hotels = new ArrayList<>();
+                String sql;
+                sql = "SELECT * FROM HotelSearch, Hotel WHERE HotelSearch.nr = Hotel.nr AND "
+                + "gym >= " + gym + " AND spa >= " + spa +
+                " AND pool >= " + pool + " AND hottub >= " + hottub +
+                " AND wifi >= " + wifi + " AND conference >= " + conference +
+                " AND restaurant >= " + restaurant + " AND bar >= " + bar +
+                " AND inclusive >= " + inclusive + " AND breakfast >= " + breakfast +
+                " AND cancellation >= " + cancellation + " AND roomService >= " + roomService +
+                " AND wheelChair >= " + wheelChair + " AND elevator >= " + elevator +
+                " AND flyBus >= " + flyBus;
+                
+                if (type != 0)
+                sql += " AND type = " + type;
+
+                try (Connection conn = connect();
+                Statement stmt  = conn.createStatement();
+                ResultSet rs    = stmt.executeQuery(sql)){
+
                 while (rs.next()) {
                     Hotel h = new Hotel(rs.getInt("nr"), rs.getInt("type"), rs.getInt("gym"), rs.getInt("spa"), 
                         rs.getInt("pool"), rs.getInt("hottub"), rs.getInt("wifi"), rs.getInt("conference"), 
@@ -92,35 +160,16 @@ public class HotelDatabaseManager {
                         getRooms(rs.getInt("nr")));
                     hotels.add(h);
                 }
-//	System.out.print("Komin í línu 125");
+
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         return hotels; 
     }
-	    
-	 
-    private ArrayList<Room> getRooms(int hnr){
-        String sql = "SELECT * FROM RoomSearch, Room WHERE RoomSearch.nr = Room.nr AND hotelnr = hnr AND hnr = " + hnr;
-        ArrayList<Room> rooms = new ArrayList<>();	
-        try (Connection conn = connect();
-        Statement stmt  = conn.createStatement();
-        ResultSet rs    = stmt.executeQuery(sql)){	     
-            while (rs.next()) {
-            	Room r = new Room(rs.getInt("nr"), rs.getInt("hnr"), rs.getInt("pets"), 
-                    rs.getInt("washing"), rs.getInt("kitchen"), rs.getInt("minifridge"),
-	            rs.getInt("tv"), rs.getInt("bath"), rs.getInt("view"), rs.getInt("view"),
-	            rs.getInt("noise"), rs.getInt("smoke"), rs.getInt("ac"), rs.getInt("price"), 
-	            rs.getInt("size"), rs.getInt("bed1"), rs.getInt("bed2"), rs.getInt("baby"));
-	            rooms.add(r);
-	    }
-		
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return rooms;	      
-    }
-    
+
+    /**
+    * Get string information for a hotel (specified by integer).
+    */
     public String[] getHotelStringInfo(int nr){
         String sql = "SELECT * FROM Hotel WHERE nr = " + nr;
         String[] hotelStringInfo = new String[3];		   
@@ -138,6 +187,9 @@ public class HotelDatabaseManager {
         return hotelStringInfo;		      
     }
 
+    /**
+    * Get int information for a hotel (specified by integer).
+    */
     public int[] getHotelIntInfo(int nr){
         String sql = "SELECT * FROM Hotel WHERE nr = " + nr;
             int[] hotelIntInfo = new int[2];		   
